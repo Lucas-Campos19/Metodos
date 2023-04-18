8dae6ba49b90510cec87bbc867223cf51f6754b2
Lucas <lucas.cgoncalves7@gmail.com>
2023-04-18 15:58:31 -0300

    Adicionar arquivos de projeto.

---
+++ Metodos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1 = int.Parse(Console.ReadLine());
+            int v2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("A soma de {0} + {1} é igual a {2}", v1, v2, Soma(v1, v2));
+            Console.ReadKey();
+        }
+
+        static int Soma(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
